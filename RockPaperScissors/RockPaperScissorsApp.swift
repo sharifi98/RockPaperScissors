8c02ac949e0d4bb2c9978c3581d062d4b179b066
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-10 15:54:31 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Hossein Sharifi on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
